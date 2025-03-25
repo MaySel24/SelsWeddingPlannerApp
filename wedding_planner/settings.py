0a24eb9059eb0d3c99f46eb1fbadbf0459d6c916
MaySel24 <doviselasi@gmail.com>
2025-03-25 21:16:51 +0000

    Initial commit

--- wedding_planner/settings.py
+++ wedding_planner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hin)-wy@mgoa*bfz@)jgj-+i4)p9tx6w(r0w&ngj!=b#s07k$+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["w"]
 
