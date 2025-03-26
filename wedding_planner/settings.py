914fedb6b7977f94b25d2bf584d6ce91b2fb8744
MaySel24 <doviselasi@gmail.com>
2025-03-26 15:22:46 +0000

    Updated settings.py and deployment configs

--- wedding_planner/settings.py
+++ wedding_planner/settings.py
@@ -63,7 +63,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")  # Use environment variable for DATABAS
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+        "default": dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
     }
 else:
     DATABASES = {
