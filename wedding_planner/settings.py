7d3a6491b2b8280d7e6cd4fcfdfef2170c540369
MaySel24 <doviselasi@gmail.com>
2025-03-27 09:50:44 +0000

    Updated ALLOWED_HOSTS for Render

--- wedding_planner/settings.py
+++ wedding_planner/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "Snatcher24")  # Load from environment in p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("selsweddingplannerapp.onrender.com", "127.0.0.1,localhost").split(",")
 
 # Application definition
 INSTALLED_APPS = [
